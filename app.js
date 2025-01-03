41c07ef6ec961bab655e67e090ed254da78e7fef
Leonel Pavon <118942980+LeoPavvv@users.noreply.github.com>
2025-01-03 13:29:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,55 @@
 import express, {json, urlencoded} from 'express'
 import cors from 'cors'
 
+import { config as dotenvConfig } from 'dotenv'
+
+import databaseConfig  from './config/database.js'
+
+dotenvConfig()
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.use(express.urlencoded({extended:true}))
 
+app.use(express.urlencoded({extended:true}))
 app.use(json())
 app.use(cors())
-
 app.disable('x-powered-by')
 
+
+const ENV = process.env.NODE_ENV || 'development'
+
+const sequelize = new Sequelize(databaseConfig[env].database, databaseConfig[env].username, databaseConfig[env].password, databaseConfig[env])
+
+sequelize.authenticate()
+    .then(() => {
+        console.log("Conexion a la base de datos realizada con exito")
+    })
+    .catch((e) => {
+        console.error("No se pudo realizar la conexion a la base de datos: ", e)
+    })
+
+
+
 app.use((req, res, next) => {
-    const token = req.cookies.access_token
-
-    if (!req.session) {
-        req.session = {}
-    }
-
-    req.session.user = null
+    const token = req.cookies?.access_token
+    req.session = req.session || { user: null }
 
     if (token) {
         try {
-            const data = jwt.verify(token, SECRET_JWT_KEY)
-            console.log("Token verificado:", data)
+            const data = jwt.verify(token, process.env.JWT_SECRET)
             req.session.user = data
         } catch (error) {
-            console.error('Error al verificar el token:', error)
+            console.error('Error al verificar el token:', error.message)
         }
     }
-
     next()
 })
 
-app.get('/', (req, res) => {
-    
-})
 
-
-app.listen(1234, () => {
-    console.log(`Servidor escuchando en http://localhost:1234`)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`)
 })
