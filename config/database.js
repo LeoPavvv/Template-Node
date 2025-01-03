afe7b8f2a5eccd8aa13aacd089e005365879c50d
Leonel Pavon <118942980+LeoPavvv@users.noreply.github.com>
2025-01-03 13:31:12 -0300

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,28 @@
+import {config} from 'dotenv'
+
+config()
+
+
+export default {
+    development: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,  // O el tipo de base de datos que estés usando (postgres, sqlite, etc.)
+    },
+    test: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    },
+    production: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    },
+}
